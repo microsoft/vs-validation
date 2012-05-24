ef7e658f3a0f0c016976ff1d3f708b5ff03ade37
Andrew Arnott <andarno@microsoft.com>
2012-05-24 09:12:02 -0700

    Enhanced buildpkg script.

--- Microsoft.Validation/buildpkg.bat
+++ Microsoft.Validation/buildpkg.bat
@@ -1,1 +1,9 @@
-nuget pack -build -symbols  -properties configuration=release -OutputDirectory bin\release 
+@ECHO OFF
+
+if "%1"=="" (
+    ECHO USAGE: %0 version
+    EXIT /b 1
+)
+
+@echo on
+nuget pack -build -symbols  -properties configuration=release -OutputDirectory bin\release -Version %1
