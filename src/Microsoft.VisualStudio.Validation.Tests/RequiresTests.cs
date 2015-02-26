135e400182b1757c0a8ac417059eaa402e41fd07
Andrew Arnott <andarno@microsoft.com>
2015-02-26 08:34:55 -0800

    Add unit test project

---
+++ src/Microsoft.VisualStudio.Validation.Tests/RequiresTests.cs
@@ -0,0 +1,14 @@
+﻿namespace Microsoft.VisualStudio.Validation.Tests
+{
+    using System;
+    using Xunit;
+
+    public class RequiresTests
+    {
+        [Fact]
+        public void NotNull_ThrowsOnNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => Requires.NotNull<object>((object)null, "foo"));
+        }
+    }
+}
