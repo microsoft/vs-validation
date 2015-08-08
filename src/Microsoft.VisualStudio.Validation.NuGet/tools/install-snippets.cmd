a960a96c6ade32b8c4ab3e82f442efcc1124ae7b
Andrew Arnott <andarno@microsoft.com>
2015-08-08 09:58:24 -0700

    Fork snippets to support older VS versions

--- src/Microsoft.VisualStudio.Validation.NuGet/tools/install-snippets.cmd
+++ src/Microsoft.VisualStudio.Validation.NuGet/tools/install-snippets.cmd
@@ -7,9 +7,16 @@ IF "%DOCS%"=="" (
 	EXIT /b 1
 )
 
-FOR %%V IN (2010 2012 2013 2015) DO (
+FOR %%V IN (2010 2012 2013) DO (
 	IF EXIST "%DOCS%\Visual Studio %%V" (
 		ECHO Installing snippets for Visual Studio %%V...
-		ROBOCOPY /NJH /NJS /NDL /NFL "%~dp0\" "%DOCS%\Visual Studio %%V\Code Snippets\Visual C#\My Code Snippets" *.snippet
+		ROBOCOPY /NJH /NJS /NDL /NFL "%~dp02013" "%DOCS%\Visual Studio %%V\Code Snippets\Visual C#\My Code Snippets" *.snippet
+	)
+)
+
+FOR %%V IN (2015) DO (
+	IF EXIST "%DOCS%\Visual Studio %%V" (
+		ECHO Installing snippets for Visual Studio %%V...
+		ROBOCOPY /NJH /NJS /NDL /NFL "%~dp02015" "%DOCS%\Visual Studio %%V\Code Snippets\Visual C#\My Code Snippets" *.snippet
 	)
 )
