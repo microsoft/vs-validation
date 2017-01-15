1b71828c5bf768ae8c233c295eb1d823fb363507
Andrew Arnott <andrewarnott@gmail.com>
2017-01-15 00:00:40 -0800

    Switch to .NET SDK project type

---
+++ src/Microsoft.VisualStudio.Validation.Tests/AssumesTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+#if NET45
+using System.Runtime.Serialization.Formatters.Binary;
+#endif
+using Microsoft;
+using Xunit;
+
+public partial class AssumesTests : IDisposable
+{
+    private const string TestMessage = "Some test message.";
+    private AssertDialogSuppression suppressAssertUi = new AssertDialogSuppression();
+
+    public void Dispose()
+    {
+        this.suppressAssertUi.Dispose();
+    }
+
+    [Fact]
+    public void True()
+    {
+        Assumes.True(true);
+        Assert.ThrowsAny<Exception>(() => Assumes.True(false, TestMessage));
+    }
+
+#if NET45
+    [Fact]
+    public void InternalErrorException_IsSerializable()
+    {
+        try
+        {
+            Assumes.False(true);
+        }
+        catch (Exception ex)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            var ms = new MemoryStream();
+            formatter.Serialize(ms, ex);
+            ms.Position = 0;
+            var ex2 = (Exception)formatter.Deserialize(ms);
+            Assert.IsType(ex.GetType(), ex2);
+            Assert.Equal(ex.Message, ex2.Message);
+        }
+    }
+#endif
+}
---
+++ src/Microsoft.VisualStudio.Validation/netstandardExtras/LocalizableAttribute.cs
@@ -0,0 +1,24 @@
+﻿/********************************************************
+*                                                        *
+*   © Copyright (C) Microsoft. All rights reserved.      *
+*                                                        *
+*********************************************************/
+
+#if NETSTANDARD1_0 || PROFILE259
+namespace System.ComponentModel
+{
+    /// <summary>
+    /// Marks whether a parameter (or other element) is meant to contain localizable text.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.All)]
+    internal sealed class LocalizableAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LocalizableAttribute"/> class.
+        /// </summary>
+        public LocalizableAttribute(bool isLocalizable)
+        {
+        }
+    }
+}
+#endif
